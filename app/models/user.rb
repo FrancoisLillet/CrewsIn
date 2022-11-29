04782385cb68a3a2fa208f76c85270968d897f65
Francois Lillet <francois.lillet@hec.edu>
2022-11-29 18:32:16 +0100

    Halfway through the seeding

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
   has_many :created_trips, class_name: "Trip", foreign_key: "creator_id"
   has_many :invitations_sent, class_name: "Invitation", foreign_key: "sender_id"
   has_many :invitations_received, class_name: "Invitation", foreign_key: "receiver_id"
+  has_many :experiences
+  has_many :mates
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
