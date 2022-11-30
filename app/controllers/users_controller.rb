47b81c0edb12f7f29a3805a5e22be8e45ca32491
grant chichester <gchichester@gmail.com>
2022-11-30 11:47:44 +0100

    migrated active_storage for cloudinary

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,12 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+
+private
+
+def user_params
+  params.require(:user).permit(:title, :body, :photo)
+end
+
+
 end
