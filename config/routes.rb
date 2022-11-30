d0c0868c743f1d15d1c90606793089b96ee8e6f5
Rodrigo Alvarez Abelleira <rodrigo.alvarez.abelleira@gmail.com>
2022-11-30 17:18:58 +0100

    STH

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :users, only: [:show, :update]
+  resources :users, only: [:show, :update] do
+    resources :mates, only: %i[index new create edit update]
+  end
+  resources :mates, only: %i[show destroy]
 end
