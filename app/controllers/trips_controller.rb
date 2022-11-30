4c022920ed467be456604a13e7c637df9d8c4c94
Francois Lillet <francois.lillet@hec.edu>
2022-11-30 17:29:53 +0100

    Added nickname to Users

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -2,8 +2,9 @@ class TripsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @user = current.user
+    @user = current_user
     @trips = @user.created_trips + @user.invitations_received
-    raise
+    @upcoming_trips = @trips.select { |trip| trip.start_date >= Date.today }
+    @past_trip = @trips.select { |trip| trip.start_date > Date.today }
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_30_104244) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_30_161955) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -120,6 +120,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_30_104244) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "nautical_bio"
+    t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
