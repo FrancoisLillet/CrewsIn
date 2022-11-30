56a6f29df25e0b7ca4ca3a1ce08aa33fa5b30045
grant chichester <gchichester@gmail.com>
2022-11-30 12:18:20 +0100

    cloudinary is functional and there is an update profile picture button on the user#show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,17 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user)
+  end
+
 
 private
 
 def user_params
-  params.require(:user).permit(:title, :body, :photo)
+  params.require(:user).permit(:photo)
 end
 
 
