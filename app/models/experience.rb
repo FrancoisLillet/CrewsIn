1e0828c88cda6b9df5b72b72067efbf3559ee538
Francois Lillet <francois.lillet@hec.edu>
2022-11-30 13:51:59 +0100

    Seeds complete

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -7,7 +7,7 @@ class Experience < ApplicationRecord
   validates :start_date, presence: true
   validates :end_date, presence: true
   validates :role, presence: true
-  validates :number_of_miles, presence: true, :numericality { only_integer: true }
+  validates :number_of_miles, presence: true, numericality: { only_integer: true }
   validates :boat_type, presence: true
   validates :boat_model, presence: true
   validates :boat_owner, presence: true
