aaaea9d0902b457bcc8370a615a11dd00efd7ff4
Gerard Almenara <ger.almenara@gmail.com>
2022-12-03 21:50:26 +0100

    added animations to sidebar

--- app/javascript/controllers/dropdown_reveal_controller.js
+++ app/javascript/controllers/dropdown_reveal_controller.js
@@ -2,9 +2,10 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="dropdown-reveal"
 export default class extends Controller {
-  static targets = ["content"]
+  static targets = ["content", "arrow"]
 
   revealContent() {
     this.contentTarget.classList.toggle("display-none")
+    this.arrowTarget.classList.toggle("flipped")
   }
 }
