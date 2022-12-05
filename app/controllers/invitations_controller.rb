f3a61699dcfccab9f36248cdb59c65b36158fd5f
grant chichester <gchichester@gmail.com>
2022-12-05 16:38:33 +0100

    created invitations index view

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,11 +1,14 @@
 class InvitationsController < ApplicationController
+
   def create
+
     @user = current_user
     @invitation = Invitation.new(invitation_params)
     @invitation.sender = @user
     @invitation.trip_id = params[:trip_id]
     @invitation.save
     if @invitation.save
+      MyMailer.with(invitation: @invitation).invitation_email.deliver_now
       redirect_to user_trips_path
     else
       redirect_to user_trips_path, alert: "Your invitation was not sent because some fields were missing"
--- app/mailers/my_mailer.rb
+++ app/mailers/my_mailer.rb
@@ -1,10 +1,8 @@
 class MyMailer < ActionMailer::Base
   default from: 'noreply@example.com'
 
-  def invitation_email(form_params, invite_url)
-    @invitation = invitation
-    @reciever_email = form_params[:reciever_email]
-    @invite_url = invite_url
-    mail(to: @reciever_email, subject: "You're invited to a trip!")
+  def invitation_email
+    @invitation = params[:invitation]
+    mail(to: @invitation.receiver_email, subject: "You're invited to a trip!")
   end
 end
